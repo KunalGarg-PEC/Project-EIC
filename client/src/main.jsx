1f747b6d7ee35522df3fa475b82a246d6d5ba805
SiddharthxsrivastavA <srivastavasiddharth2003@gmail.com>
2024-04-05 22:18:53 +0530

    Revert "sid commit for testing"
    This reverts commit 54f64c3e3fd68326d1bb5399513439f0e310752d.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { persistor, store } from './redux/store.js';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// HELLO FROM SIDDHARTH
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
-);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
