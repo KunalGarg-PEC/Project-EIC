c0f967f5ca3194d1c31683d9548acf981e8d4ee1
Alok Mehta <alokmehta2565@gmail.com>
2024-04-06 01:37:03 +0530

    complete backend routes of owner and resident

--- api/Routes/user.js
+++ api/Routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const userRouter = express.Router();
+const {ownerRouter} = require('./owner');
+const {residentRouter} = require('./resident');
 
 userRouter.use('/owner', ownerRouter);
 userRouter.use('/resident', residentRouter);
