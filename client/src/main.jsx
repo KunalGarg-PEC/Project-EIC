0f45cfdc5956622e216745a1ebc69f28138fc2a4
SiddharthxsrivastavA <srivastavasiddharth2003@gmail.com>
2024-04-05 17:48:18 +0530

    HELLO FROM SID

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+// HELLO FROM SIDDHARTH
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
